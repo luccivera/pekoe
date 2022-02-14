a458d9b9b5da11d99645b225efe1fa075c8c8e0f
Aleisha Silvera <aleishasilvera@me.com>
2022-02-13 22:40:20 -0500

    added necessary custom core blocks

--- themes/pko/assets/js/block-editor.js
+++ themes/pko/assets/js/block-editor.js
@@ -1,16 +1,225 @@
 console.log("Block Editor");
 
-wp.blocks.registerBlockStyle("core/quote", {
-  name: "fancy-quote",
-  label: "Fancy Quote",
+wp.blocks.registerBlockStyle("core/archives", {
+  name: "pko-archives",
+  label: "Pko Archives",
+});
+wp.blocks.registerBlockStyle("core/audio", {
+  name: "pko-audio",
+  label: "Pko Audio",
+});
+
+wp.blocks.registerBlockStyle("core/button", {
+  name: "pko-button",
+  label: "Pko Button",
+});
+wp.blocks.registerBlockStyle("core/buttons", {
+  name: "pko-buttons",
+  label: "Pko Buttons",
+});
+wp.blocks.registerBlockStyle("core/calendar", {
+  name: "pko-calendar",
+  label: "Pko Calendar",
+});
+wp.blocks.registerBlockStyle("core/categories", {
+  name: "pko-categories",
+  label: "Pko Categories",
+});
+wp.blocks.registerBlockStyle("core/code", {
+  name: "pko-code",
+  label: "Pko Code",
+});
+wp.blocks.registerBlockStyle("core/column", {
+  name: "pko-column",
+  label: "Pko Column",
+});
+wp.blocks.registerBlockStyle("core/columns", {
+  name: "pko-columns",
+  label: "Pko Columns",
+});
+wp.blocks.registerBlockStyle("core/comment-author-name", {
+  name: "pko-comment-author-name",
+  label: "Pko Comment Author Name",
+});
+wp.blocks.registerBlockStyle("core/comment-content", {
+  name: "pko-comment-content",
+  label: "Pko Comment Content",
+});
+wp.blocks.registerBlockStyle("core/comment-date", {
+  name: "pko-comment-date",
+  label: "Pko Comment Date",
+});
+wp.blocks.registerBlockStyle("core/comment-edit-link", {
+  name: "pko-comment-edit-link",
+  label: "Pko Comment Edit Link",
+});
+wp.blocks.registerBlockStyle("core/comment-template", {
+  name: "pko-comment-template",
+  label: "Pko Comment Template",
+});
+wp.blocks.registerBlockStyle("core/comments-pagination", {
+  name: "pko-comment-pagination",
+  label: "Pko Comment Pagination",
+});
+wp.blocks.registerBlockStyle("core/comments-pagination-next", {
+  name: "pko-comment-pagination-next",
+  label: "Pko Comment Pagination Next",
+});
+wp.blocks.registerBlockStyle("core/comments-pagination-numbers", {
+  name: "pko-comment-pagination-numbers",
+  label: "Pko Comment Pagination Numbers",
+});
+wp.blocks.registerBlockStyle("core/comments-previous", {
+  name: "pko-comment-previous",
+  label: "Pko Comment previous",
+});
+wp.blocks.registerBlockStyle("core/comment-template", {
+  name: "pko-comments-template",
+  label: "Pko Comments Template",
+});
+
+wp.blocks.registerBlockStyle("core/comment-template", {
+  name: "pko-comment-template",
+  label: "Pko Comment Template",
+});
+wp.blocks.registerBlockStyle("core/comment-template", {
+  name: "pko-comment-template",
+  label: "Pko Comment Template",
+});
+wp.blocks.registerBlockStyle("core/comment-template", {
+  name: "pko-comment-template",
+  label: "Pko Comment Template",
+});
+wp.blocks.registerBlockStyle("core/comment-template", {
+  name: "pko-comment-template",
+  label: "Pko Comment Template",
+});
+wp.blocks.registerBlockStyle("core/code", {
+  name: "pko-code",
+  label: "Pko Code",
+});
+
+wp.blocks.registerBlockStyle("core/code", {
+  name: "pko-code",
+  label: "Pko Code",
+});
+wp.blocks.registerBlockStyle("core/cover", {
+  name: "pko-cover",
+  label: "Pko Cover",
+});
+wp.blocks.registerBlockStyle("core/file", {
+  name: "pko-file",
+  label: "Pko File",
+});
+wp.blocks.registerBlockStyle("core/gallery", {
+  name: "pko-gallery",
+  label: "Pko Gallery",
+});
+wp.blocks.registerBlockStyle("core/heading", {
+  name: "pko-heading",
+  label: "Pko Heading",
+});
+wp.blocks.registerBlockStyle("core/html", {
+  name: "pko-html",
+  label: "Pko Html",
+});
+wp.blocks.registerBlockStyle("core/image", {
+  name: "pko-image",
+  label: "Pko Image",
+});
+wp.blocks.registerBlockStyle("core/latest-comments", {
+  name: "pko-latest-comments",
+  label: "Pko Latest Comments",
+});
+wp.blocks.registerBlockStyle("core/latest-posts", {
+  name: "pko-latest-posts",
+  label: "Pko Latest Posts",
+});
+wp.blocks.registerBlockStyle("core/list", {
+  name: "pko-list",
+  label: "Pko List",
+});
+wp.blocks.registerBlockStyle("core/media-text", {
+  name: "pko-media-text",
+  label: "Pko Media Text",
+});
+wp.blocks.registerBlockStyle("core/more", {
+  name: "pko-more",
+  label: "Pko More",
+});
+wp.blocks.registerBlockStyle("core/navigation", {
+  name: "pko-navigation",
+  label: "Pko Navigation",
+});
+wp.blocks.registerBlockStyle("core/navigation-link", {
+  name: "pko-navigation-link",
+  label: "Pko Navigation Link",
+});
+wp.blocks.registerBlockStyle("core/nextpage", {
+  name: "pko-next-page",
+  label: "Pko Next Page",
 });
 
 wp.blocks.registerBlockStyle("core/paragraph", {
   name: "pko-paragraph",
   label: "Pko Paragraph",
 });
+wp.blocks.registerBlockStyle("core/preformatted", {
+  name: "pko-preformatted",
+  label: "Pko Preformatted",
+});
+wp.blocks.registerBlockStyle("core/pullquote", {
+  name: "pko-pullquote",
+  label: "Pko Pullquote",
+});
+wp.blocks.registerBlockStyle("core/quote", {
+  name: "pko-quote",
+  label: "Pko Quote",
+});
+wp.blocks.registerBlockStyle("core/rss", {
+  name: "pko-rss",
+  label: "Pko RSS",
+});
+wp.blocks.registerBlockStyle("core/search", {
+  name: "pko-search",
+  label: "Pko Search",
+});
+wp.blocks.registerBlockStyle("core/separator", {
+  name: "pko-separator",
+  label: "Pko Separator",
+});
+wp.blocks.registerBlockStyle("core/social-link", {
+  name: "pko-social-link",
+  label: "Pko Social Link",
+});
+wp.blocks.registerBlockStyle("core/social-links", {
+  name: "pko-social-links",
+  label: "Pko Social Links",
+});
+wp.blocks.registerBlockStyle("core/spacer", {
+  name: "pko-spacer",
+  label: "Pko Spacer",
+});
+wp.blocks.registerBlockStyle("core/tag-cloud", {
+  name: "pko-tag-cloud",
+  label: "Pko Tag Cloud",
+});
+wp.blocks.registerBlockStyle("core/table", {
+  name: "pko-table",
+  label: "Pko Table",
+});
+wp.blocks.registerBlockStyle("core/verse", {
+  name: "pko-verse",
+  label: "Pko Verse",
+});
+wp.blocks.registerBlockStyle("core/video", {
+  name: "pko-video",
+  label: "Pko Video",
+});
 
 wp.domReady(function () {
   wp.blocks.unregisterBlockStyle("core/quote", "large");
   wp.blocks.unregisterBlockStyle("core/quote", "plain");
+  wp.blocks.unregisterBlockStyle("core/button", "default");
+  wp.blocks.unregisterBlockStyle("core/button", "outline");
 });
