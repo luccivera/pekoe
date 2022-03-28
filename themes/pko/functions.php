d103de3e1f401e251bfda36e4e2dd47bb1043dd6
Aleisha Silvera <aleishasilvera@me.com>
2022-03-27 22:17:52 -0400

    added custom post, tea template and tea single. updated wc and wp

--- themes/pko/functions.php
+++ themes/pko/functions.php
@@ -239,6 +239,7 @@ function activate_gutenberg_product( $can_edit, $post_type ) {
     return $can_edit;
 }
 add_filter( 'use_block_editor_for_post_type', 'activate_gutenberg_product', 10, 2 );
+
 /**
  * Custom template tags for this theme.
  */
@@ -263,3 +264,57 @@ require get_template_directory() . '/inc/block-editor.php';
  * Custom Post Type
  */
 require get_template_directory() . '/inc/post-type.php';
+
+/* Custom Post Type Start */
+
+
+function your_prefix_register_post_type() {
+	$args = [
+		'label'  => esc_html__( 'Teas', 'text-domain' ),
+		'labels' => [
+			'menu_name'          => esc_html__( 'Teas', 'your-textdomain' ),
+			'name_admin_bar'     => esc_html__( 'Tea', 'your-textdomain' ),
+			'add_new'            => esc_html__( 'Add Tea', 'your-textdomain' ),
+			'add_new_item'       => esc_html__( 'Add new Tea', 'your-textdomain' ),
+			'new_item'           => esc_html__( 'New Tea', 'your-textdomain' ),
+			'edit_item'          => esc_html__( 'Edit Tea', 'your-textdomain' ),
+			'view_item'          => esc_html__( 'View Tea', 'your-textdomain' ),
+			'update_item'        => esc_html__( 'View Tea', 'your-textdomain' ),
+			'all_items'          => esc_html__( 'All Teas', 'your-textdomain' ),
+			'search_items'       => esc_html__( 'Search Teas', 'your-textdomain' ),
+			'parent_item_colon'  => esc_html__( 'Parent Tea', 'your-textdomain' ),
+			'not_found'          => esc_html__( 'No Teas found', 'your-textdomain' ),
+			'not_found_in_trash' => esc_html__( 'No Teas found in Trash', 'your-textdomain' ),
+			'name'               => esc_html__( 'Teas', 'your-textdomain' ),
+			'singular_name'      => esc_html__( 'Tea', 'your-textdomain' ),
+		],
+		'public'              => true,
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'show_ui'             => true,
+		'show_in_nav_menus'   => true,
+		'show_in_admin_bar'   => true,
+		'show_in_rest'        => true,
+		'capability_type'     => 'post',
+		'hierarchical'        => false,
+		'has_archive'         => true,
+		'query_var'           => true,
+		'can_export'          => true,
+		'rewrite_no_front'    => false,
+		'show_in_menu'        => true,
+		'supports' => [
+			'title',
+			'editor',
+			'thumbnail',
+		],
+		'taxonomies' => [
+			'category',
+			'tag',
+		],
+		'rewrite' => true
+	];
+
+	register_post_type( 'tea', $args );
+}
+add_action( 'init', 'your_prefix_register_post_type' );
+	/* Custom Post Type End */
