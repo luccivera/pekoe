1f322c2accd32bad43d69e33b5ace7c6d2299289
Aleisha Silvera <aleishasilvera@me.com>
2022-03-29 10:10:53 -0400

    added div to template file

--- themes/pko/template-teas.php
+++ themes/pko/template-teas.php
@@ -1,15 +1,23 @@
 <?php
 /*Template Name: Teas*/
 get_header();
+?>
+<div class="tea-post-temp-content">
+<?php
+
 query_posts(array(
    'post_type' => 'tea'
 )); ?>
+
 <?php
 while (have_posts()) : the_post(); ?>
+<div class="tea-post-content">
 <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
 <p><?php the_excerpt(); ?></p>
+</div>
+
 <?php endwhile;
 get_footer();
 ?>
 
-$
+
